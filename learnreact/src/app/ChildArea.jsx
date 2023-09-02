21d74d70ff527ab1c85885a13cc629086c1f613d
Sozai83 <chikushi3.227@gmail.com>
2023-09-02 22:39:33 +1000

    tested re-rednering

--- learnreact/src/app/ChildArea.jsx
+++ learnreact/src/app/ChildArea.jsx
@@ -1,5 +1,11 @@
 export const ChildArea = (props) => {
   const { open } = props;
+  console.group("ChildArea got rendered.")
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log(".....");
+  })
+
   return (
     <>
       {open ? (<div>
