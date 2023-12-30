70b3582f8b129d151b387ed62b67aa70f9a51acb
Souzai83 <chikushi3.227@gmail.com>
2023-12-30 14:42:42 +1100

    testing jsonplaceholder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import {Router} from './components/router/Router'
 import { UserProvider } from './providers/UserProvider';
 import { RecoilRoot } from 'recoil';
+import axios from "axios";
 
 function App() {
+
+  const onClickUsers = ()=>{
+    axios
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>{console.log(res.data)})
+    .catch((err)=>console.error(err))
+  }
+
+  const onClickUser1 = ()=>{
+    axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+    .then((res)=>{console.log(res.data)})
+    .catch((err)=>console.error(err))
+  }
+
+
   return (
     <RecoilRoot>
       <UserProvider>
         <Router/>
+        <button onClick={onClickUsers}>Users</button>
+        <button onClick={onClickUser1}>User 1</button>
       </UserProvider>
     </RecoilRoot>
   );
